e77b7a31092c277fb5f2fde9f7d105e677edad65
bharghavbaddam <bharghavbaddam@192.168.0.9>
2020-06-01 16:05:11 -0400

    Implemented Pagination and Year Query Parameters

--- src/main/java/org/bharghav/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/bharghav/javabrains/messenger/resources/MessageResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.bharghav.javabrains.messenger.model.Message;
@@ -36,8 +37,16 @@ public class MessageResource {
 //		return messageService.getMessage(id);
 //	}
 	
-	@GET
-	public List<Message> getMessages() {
+	@GET// Adding query param annotation for retrieving the messages according to year parameter and also start and size of pagination
+	public List<Message> getMessages(@QueryParam("year") int year,
+									@QueryParam("start") int start,
+									@QueryParam("size") int size) {
+		if(year > 0) {
+			return messageService.getAllMessagesForYear(year);
+		}
+		if(start > 0 && size > 0) {
+			return messageService.getAllMessagesPaginated(start, size);
+		}
 		return messageService.getAllMessages();
 	}
 	
