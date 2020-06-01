8d290b4ef968b62e56719ab0d0fa1ed23ac42725
bharghavbaddam <bharghavbaddam@192.168.0.9>
2020-06-01 14:58:48 -0400

    Update and Delete methods are written

--- src/main/java/org/bharghav/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/bharghav/javabrains/messenger/resources/MessageResource.java
@@ -2,7 +2,11 @@ package org.bharghav.javabrains.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,6 +16,8 @@ import org.bharghav.javabrains.messenger.model.Message;
 import org.bharghav.javabrains.messenger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)//Produces and Consumes are done at class level to be applied for each method
+@Produces(MediaType.APPLICATION_JSON)// Infact these two can be written for each method individually but since every method consumes or produces this is efficient
 public class MessageResource {
 	
 	MessageService messageService = new MessageService();
@@ -31,14 +37,32 @@ public class MessageResource {
 //	}
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)// JSON message is converted to Message instance by Jersey
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
+	}
+	
 	@GET
 	@Path("/{messageId}")// here message id comes in string format but "long" in line 28 converts it to long automatically
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long id) {
 		
 		return messageService.getMessage(id);
